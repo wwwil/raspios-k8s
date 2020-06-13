bfa2b81b928841b34902002cd332e086add9cc8f
wwwil <wwwil.squires@gmail.com>
2020-06-13 14:35:34 +0100

    Rename built image and export environment variable

--- build.sh
+++ build.sh
@@ -11,11 +11,16 @@ if [ ! -f "$IMAGE_ZIP" ]; then
    wget -nv $IMAGE_LINK
 fi
 unzip -o $IMAGE_ZIP
+IMAGE_NAME=${IMAGE_ZIP%.zip}.img
 
 docker run --privileged --rm \
   -e MOUNT="/raspios-k8s" \
-  -e SOURCE_IMAGE="/raspios-k8s/${IMAGE_ZIP%.zip}.img" \
+  -e SOURCE_IMAGE="/raspios-k8s/${IMAGE_NAME}" \
   -e SCRIPT="/raspios-k8s/setup.sh" \
   -e ADD_DATA_PART="false" \
   --mount type=bind,source="$(pwd)",destination=/raspios-k8s \
   lumastar/raspbian-customiser:local-test
+
+# Rename the image and export the name for use with other scripts.
+export RASPIOS_K8S_IMAGE_NAME="${IMAGE_NAME%.img}.raspios-k8s.img"
+mv $IMAGE_NAME $RASPIOS_K8S_IMAGE_NAME
