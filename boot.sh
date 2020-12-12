b7344af53803fea52e8fae80ea6c3c464ee7ba38
wwwil <wwwil.squires@gmail.com>
2020-12-12 14:47:40 +0000

    Fix syntax error with if block, print info, disable xtrace

--- boot.sh
+++ boot.sh
@@ -3,20 +3,22 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-set -o xtrace
+
+# Useful for debug.
+#set -o xtrace
 
 # raspios-k8s boot script to be run by rc.local.
 
 # Copy SSH public key from /boot/.
 if [ -f /boot/id_rsa.pub ]; then
   # Create SSH directory if not present.
-  if ![ -d /home/pi/.ssh ]; then
+  if [ ! -d /home/pi/.ssh ]; then
     mkdir -p /home/pi/.ssh
     chmod 700 /home/pi/.ssh
     chown pi:pi /home/pi/.ssh
   fi
   # Create authorized keys file if not present.
-  if ![ -f /home/pi/.ssh/authorized_keys ]; then
+  if [ ! -f /home/pi/.ssh/authorized_keys ]; then
     touch /home/pi/.ssh/authorized_keys
     chmod 700 /home/pi/.ssh
     chown pi:pi /home/pi/.ssh
@@ -28,5 +30,9 @@ fi
 # TODO: Set static IP from config file in /boot.
 # TODO: Set hostname from config file in /boot.
 # TODO: Run kubeadm using config in /boot.
+
+# Print information.
+echo "Hostname: $(hostname)"
+echo "IP addresses: $(hostname- I)"
 
 exit 0
